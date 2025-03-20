555083c810bcf17b02221b3609870ce63f4f134b
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-03-20 22:47:35 +0600

    Update 0fb.js

--- scripts/cmds/0fb.js
+++ scripts/cmds/0fb.js
@@ -48,7 +48,7 @@ module.exports.onChat = async ({ bot, msg }) => {
       );
         
       const videoBuffer = (
-        await axios.get(res.data.videos.url, { responseType: "arraybuffer" })
+        await axios.get(res.data.videos.hd.url, { responseType: "arraybuffer" })
       ).data;
 
       fs.writeFileSync(videoPath, Buffer.from(videoBuffer, "utf-8"));
