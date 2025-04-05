56d199821bbb55e1bc360188759967318c2ef51b
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-04-05 07:59:43 +0600

    Update pic.js

--- scripts/cmds/pic.js
+++ scripts/cmds/pic.js
@@ -38,8 +38,8 @@ module.exports.onStart = async function ({ api, event, args, message }) {
         }
 
         await message.stream({
-            attachment: data[0] || "https://i.pinimg.com/originals/a5/05/a9/a505a99a9802e0e65f864aa7572ce995.jpg",
-            caption: ` search results for keyword: ${keySearchs}`,
+            attachment: "https://i.pinimg.com/originals/a5/05/a9/a505a99a9802e0e65f864aa7572ce995.jpg",
+            caption: `search results for keyword: ${keySearchs}`,
         });
 
     } catch (error) {
