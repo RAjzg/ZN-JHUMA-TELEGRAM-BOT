f4f54a6d1aa11ec4562ba29addb6688be62d43e8
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-04-13 17:48:52 +0600

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -24,7 +24,7 @@ module.exports.onStart = async ({ api, event, args, message }) => {
 
     const imgurResponse = await axios.get(`${Shaon}/imgur?link=${encodeURIComponent(imageUrl)}`);
 
-    message.reply(`✅Imgur link:\n${imgurResponse.data.uploaded.status}`);
+    message.reply(`✅Imgur link:\n${imgurResponse.data.uploaded.link}`);
 
   } catch (e) {
     console.log(e);
