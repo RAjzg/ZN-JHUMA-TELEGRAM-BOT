fd3e4b430f063f434333adf22faacc4edf4b2646
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-05-02 21:02:13 +0600

    Update ytdown.js

--- scripts/cmds/ytdown.js
+++ scripts/cmds/ytdown.js
@@ -53,7 +53,7 @@ module.exports.onChat = async ({ bot, msg }) => {
         `${await baseApiUrl()}/ytdown?url=${encodeURIComponent(messageText)}`
       );
       const videoBuffer = (
-        await axios.get(data.data.downloads.data.fileUrl, { responseType: "arraybuffer" })
+        await axios.get(data.downloads.data.fileUrl, { responseType: "arraybuffer" })
       ).data;
 
       fs.writeFileSync(videoPath, Buffer.from(videoBuffer, "utf-8"));
