45f2b64da5a533976031cb1c6dac631f9a83f0bf
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-05-12 08:28:51 +0600

    Update autodown.js

--- scripts/cmds/autodown.js
+++ scripts/cmds/autodown.js
@@ -60,7 +60,7 @@ module.exports.onChat = async ({ event,bot, msg }) => {
       const videoPath = path.join(__dirname, "caches", "diptoo.mp4");
 
       const { data } = await axios.get(
-        `${await baseApiUrl()}/yt?url=${encodeURIComponent(messageText)}`
+        `${await baseApiUrl()}/alldown?url=${encodeURIComponent(messageText)}`
       );
       const videoBuffer = (
         await axios.get(data.url, { responseType: "arraybuffer" })
