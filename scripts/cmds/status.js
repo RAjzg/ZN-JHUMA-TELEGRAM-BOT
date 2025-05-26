b49f0e426f75a555ec8a736f79ac1ce1155691ed
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-05-26 13:04:48 +0600

    Update status.js

--- scripts/cmds/status.js
+++ scripts/cmds/status.js
@@ -10,22 +10,34 @@ module.exports.config = {
   cooldowns: 30,
 };
 
-module.exports.run = async function({ api, message }) {
-  const axios = require('axios');
-  const request = require('request');
+module.exports.run = async function ({ api, message }) {
+try{
+  const axios = require("axios");
   const fs = require("fs");
-  const {data} = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const { data } = await axios.get(
+    "https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json",
+  );
   const video = data.api;
   var shaon = [
-    `${video}/video/status2`,
+`${video}/video/status2`,
     `${video}/video/status3`,
     `${video}/video/status4`
-]
-  var shaon1 = shaon[Math.floor(Math.random() * shaon.length)]
-  axios.get(shaon1).then(res => {
-message.stream({
-url: res.data.url.url,
-caption: `°\n\n__${res.data.url.title}\n\n🍂𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁 : 𝚂𝙷𝙰𝙾𝙽 𝙰𝙷𝙼𝙴𝙳...🌸`
-});
-      })
+               ];
+  var shaon1 = shaon[Math.floor(Math.random() * shaon.length)];
+  const res = await axios.get(shaon1);
+  const vidUrl = res.data.url.url;
+  const vid = await axios.get(vidUrl, {
+    responseType: "arraybuffer",
+    headers: { "User-Agent": "Mozila/5.0" },
+  });
+  const filep = __dirname + "/caches/video.mp4";
+  fs.writeFileSync(filep, Buffer.from(vid.data,"utf-8"));
+  message.stream({
+    url: fs.createReadStream(filep),
+    caption: `°\n\n__${res.data.url.title}\n\n🍂𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁 : 𝚂𝙷𝙰𝙾𝙽 𝙰𝙷𝙼𝙴𝙳...🌸`,
+  });
+  //fs.unlinkSync(filep);
+} catch(e){
+message.reply(e.message)
 }
+};
