36c7cb8da42fb4dd7c687df62a02401d8c576ed8
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-05-26 13:33:53 +0600

    Create rndm.js

---
+++ scripts/cmds/rndm.js
@@ -0,0 +1,40 @@
+module.exports.config = {
+  name: "rndm",
+  version: "11.9.7",
+  role: 0,
+  credits: "Islamick Cyber Chat",
+  prefix: true,
+  description: "random love story video",
+  category: "video",
+  usages: "random",
+  cooldowns: 30,
+};
+
+module.exports.run = async function ({ event, args, api, message }) {
+try{
+  const axios = require("axios");
+  const fs = require("fs");
+  const nameParam = args.join("");
+  if (!args[0]) return api.sendMessage("[ ! ] Input Name.\nEx: /rndm Shaon", event.from.ID, event.messageID);
+  const { data } = await axios.get(
+    "https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json",
+  );
+  const video = data.api;
+  const res = await axios.get(`${n}/video/random?name=${encodeURIComponent(nameParam)}`);
+  
+   const vidUrl = res.data.url;
+  const vid = await axios.get(vidUrl, {
+    responseType: "arraybuffer",
+    headers: { "User-Agent": "Mozila/5.0" },
+  });
+  const filep = __dirname + "/caches/video.mp4";
+  fs.writeFileSync(filep, Buffer.from(vid.data,"utf-8"));
+  message.stream({
+    url: fs.createReadStream(filep),
+    caption: `${res.data.cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${res.data.count}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${res.data.name}]`,
+  });
+  //fs.unlinkSync(filep);
+} catch(e){
+message.reply(e.message)
+}
+};
