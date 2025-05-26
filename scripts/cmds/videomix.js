4b1c046b3102ddb1ce856eee48257d095f1c2182
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-05-26 12:59:59 +0600

    Update videomix.js

--- scripts/cmds/videomix.js
+++ scripts/cmds/videomix.js
@@ -3,20 +3,23 @@ module.exports.config = {
   version: "11.9.7",
   role: 0,
   credits: "Islamick Cyber Chat",
-  prefix:true,
-  description: "random love story video",
+  prefix: true,
+  description: "videomix love story video",
   category: "video",
-  usages: "random",
+  usages: "videomix",
   cooldowns: 30,
 };
 
-module.exports.run = async function({ api, message }) {
-  const axios = require('axios');
-  const request = require('request');
+module.exports.run = async function ({ api, message }) {
+try{
+  const axios = require("axios");
   const fs = require("fs");
-  const {data} = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const { data } = await axios.get(
+    "https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json",
+  );
   const video = data.api;
-  var shaon = [`${video}/video/status`,
+  var shaon = [
+`${video}/video/status`,
 `${video}/video/sad`,
 `${video}/video/baby`,
 `${video}/video/love`,
@@ -35,19 +38,29 @@ module.exports.run = async function({ api, message }) {
 `${video}/video/football`,               
 `${video}/video/funny`,
 `${video}/video/sex`,
-               `${video}/video/hot`,
-               `${video}/video/item`,
-               `${video}/video/capcut`,
-               `${video}/video/sex2`,
-               `${video}/video/sex3`,
-               `${video}/video/horny`,
-]
-  var shaon1 = shaon[Math.floor(Math.random() * shaon.length)]
-  axios.get(shaon1).then(res => {
-message.stream({
-url: res.data.data,
-caption: `𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 
-${res.data.shaon}\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:${res.data.count}...🎬\n\n｢𝐒𝐇𝐀𝐎𝐍 𝐏𝐑𝐎𝐉𝐄𝐂𝐓｣`
-});
-      })
+`${video}/video/hot`,
+`${video}/video/item`,
+`${video}/video/capcut`,
+`${video}/video/sex2`,
+`${video}/video/sex3`,
+`${video}/video/horny`
+               ];
+  var shaon1 = shaon[Math.floor(Math.random() * shaon.length)];
+  const res = await axios.get(shaon1);
+  const vidUrl = res.data.url;
+  const vid = await axios.get(vidUrl, {
+    responseType: "arraybuffer",
+    headers: { "User-Agent": "Mozila/5.0" },
+  });
+  const filep = __dirname + "/caches/video.mp4";
+  fs.writeFileSync(filep, Buffer.from(vid.data,"utf-8"));
+  message.stream({
+    url: fs.createReadStream(filep),
+    caption: `𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 
+${res.data.shaon}\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:${res.data.count}...🎬\n\n｢𝐒𝐇𝐀𝐎𝐍 𝐏𝐑𝐎𝐉𝐄𝐂𝐓｣`,
+  });
+  //fs.unlinkSync(filep);
+} catch(e){
+message.reply(e.message)
 }
+};
