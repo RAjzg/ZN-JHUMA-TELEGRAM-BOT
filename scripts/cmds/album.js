a5995e17f5169d78bb00c4d94b29bdb3f669f74e
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-06-02 21:50:06 +0600

    Update album.js

--- scripts/cmds/album.js
+++ scripts/cmds/album.js
@@ -5,79 +5,82 @@ module.exports = {
     name: "album",
     version: "1.0.0",
     role: 0,
-    author: "Dipto", //Don't Change Author name.
+    author: "Dipto", // Don't change author name.
     description: "Displays album options for selection.",
     category: "Media",
     countDown: 5,
-    },
-  
-  run: async ({ event, bot, message, adminBatton}) => {
-   
-   const { msg } = event; 
-  const chatId = message.chat.id;
+  },
 
-  const videoSelectionMarkup = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Love', callback_data: '/video/love' },
-          { text: 'CPL', callback_data: '/video/cpl' }
-        ],
-        [
-          { text: 'Short Video', callback_data: '/video/shortvideo' },
-          { text: 'Sad Video', callback_data: '/video/sadvideo' }
-        ],
-        [
-          { text: 'Status', callback_data: '/video/status' },
-          { text: 'Shairi', callback_data: '/video/shairi' }
-        ],
-        [
-          { text: 'Baby', callback_data: '/video/baby' },
-          { text: 'Anime', callback_data: '/video/anime' }
-        ],
-        [
-          { text: 'Humaiyun', callback_data: '/video/humaiyun' },
-          { text: 'Islam', callback_data: '/video/islam' }
-        ],
-        [
-          { text: 'Horny', callback_data: '/video/horny' },
-          { text: 'Hot', callback_data: '/video/hot' }
-        ],
-        [
-          { text: 'Random', callback_data: '/video/random' }
+  onStart: async ({ event, bot, api }) => {
+    const chatId = event.chat.id;
+
+    const videoSelectionMarkup = {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: 'Love', callback_data: '/video/love' },
+            { text: 'CPL', callback_data: '/video/cpl' }
+          ],
+          [
+            { text: 'Short Video', callback_data: '/video/shortvideo' },
+            { text: 'Sad Video', callback_data: '/video/sadvideo' }
+          ],
+          [
+            { text: 'Status', callback_data: '/video/status' },
+            { text: 'Shairi', callback_data: '/video/shairi' }
+          ],
+          [
+            { text: 'Baby', callback_data: '/video/baby' },
+            { text: 'Anime', callback_data: '/video/anime' }
+          ],
+          [
+            { text: 'Humaiyun', callback_data: '/video/humaiyun' },
+            { text: 'Islam', callback_data: '/video/islam' }
+          ],
+          [
+            { text: 'Horny', callback_data: '/video/horny' },
+            { text: 'Hot', callback_data: '/video/hot' }
+          ],
+          [
+            { text: 'Random', callback_data: '/video/random' }
+          ]
         ]
-      ]
-    }
-  };
+      }
+    };
 
-  const waitMsg = await message.reply(chatId, "Select Video Type", videoSelectionMarkup);
+    const waitMsg = await api.sendMessage(chatId, "Select Video Type", videoSelectionMarkup);
 
-  api.once('callback_query', async (callbackQuery) => {
-    const name = callbackQuery.data;
-    await message.reply(chatId, waitMsg.message_id);
+    bot.once('callback_query', async (callbackQuery) => {
+      const name = callbackQuery.data;
 
-    const waitVoiceMsg = await message.reply(chatId, "Please wait...", { reply_to_message_id: message.message_id });
+      await api.answerCallbackQuery(callbackQuery.id);
 
-    try {
-      const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json');
-      
-  const Shaon = apis.data.api;
+      const waitVoiceMsg = await api.sendMessage(chatId, "⏳ Please wait...", {
+        reply_to_message_id: waitMsg.message_id
+      });
 
-      const data = await axios.get`${Shaon}${name}`;
-      
-      console.log(data.data);
-      const url = data.data || data.url;
-      const caption = data.shaon || data.cp;
+      try {
+        const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json');
+        const Shaon = apis.data.api;
 
-      
+        const res = await axios.get(`${Shaon}${name}`);
+        const videoUrl = res.data.url || res.data.data;
+        const caption = res.data.cp || res.data.shaon || "";
 
-      await message.reply(chatId, url, { caption: caption, reply_to_message_id: message.message_id, ...adminBatton });
-      await bot.deleteMessage(chatId, waitVoiceMsg.message_id);
-    } catch (error) {
-      await bot.deleteMessage(chatId, waitVoiceMsg.message_id);
-      console.error('Error getting file info:', error);
-      message.reply(chatId, "❌ Failed to fetch video. Try again later.", { reply_to_message_id: message.message_id });
-    }
-  });
+        await api.sendVideo(chatId, videoUrl, {
+          caption,
+          reply_to_message_id: waitMsg.message_id,
+          ...adminBatton
+        });
+
+        await api.deleteMessage(chatId, waitVoiceMsg.message_id);
+      } catch (error) {
+        console.error("❌ Error fetching video:", error.message);
+        await api.deleteMessage(chatId, waitVoiceMsg.message_id);
+        await api.sendMessage(chatId, "❌ Failed to fetch video. Try again later.", {
+          reply_to_message_id: waitMsg.message_id
+        });
+      }
+    });
   }
 };
