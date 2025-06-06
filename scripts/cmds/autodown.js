ab7e7a83a0a78cf330b474453b433848aa213410
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-06-06 14:20:43 +0600

    Update autodown.js

--- scripts/cmds/autodown.js
+++ scripts/cmds/autodown.js
@@ -6,7 +6,7 @@ const baseApiUrl = async () => {
   const base = await axios.get(
 `https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json`
   );
-  return base.data.allapi;
+  return base.data.noobs;
 };
 
 module.exports.config = {
