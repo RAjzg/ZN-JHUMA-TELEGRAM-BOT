75e8a95b9002b929195a7d7c3a960f59b2337da0
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-06-10 16:55:16 +0600

    Update getlink.js

--- scripts/cmds/getlink.js
+++ scripts/cmds/getlink.js
@@ -1,10 +1,10 @@
-const shortenURL = require("tinyurl").shorten;
 const { get } = require("axios");
+const { shorten } = require("tinyurl");
 const baseApiUrl = async () => {
-  const base = await get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  const res = await get(
+    "https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json"
   );
-  return base.data.api;
+  return res.data.api;
 };
 
 module.exports = {
@@ -12,218 +12,123 @@ module.exports = {
     name: "getlink",
     aliases: ["gl", "g"],
     version: "1.0",
-    author: "ASIF",
+    author: "Shaon Ahmed",
     countDown: 2,
     role: 0,
-    description: "𝗚𝗲𝘁 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝘂𝗿𝗹 𝗳𝗿𝗼𝗺 𝘃𝗶𝗱𝗲𝗼, 𝗮𝘂𝗱𝗶𝗼 𝘀𝗲𝗻𝘁 𝗳𝗿𝗼𝗺 𝗴𝗿𝗼𝘂𝗽",
+    description:
+      "𝗚𝗲𝘁 download URL from video/audio sent in group",
     category: "𝗨𝗧𝗜𝗟𝗜𝗧𝗬",
-    guide:"{pn} [--t/t/tinyurl] [reply_attachment]\n{pn} [--i/i/imgbb] [reply_attachment]\n{pn} [--tg/tg/telegraph] [reply_attachment]\n{pn} [reply_attachment]\n{pn} [--p/postimg/postimage] [reply_attachment]\n{pn} [--dc/-d/dc] reply or add link image\n{pn} [--sl/s/shortlink] [reply_attachment]\n{pn} [imgur/imgurl] [reply_attachment]"
+    guide:
+      "{pn} [--t] [reply_attachment]\n{pn} [--i] [reply_attachment]\n{pn} [--tg] [reply_attachment]\n{pn} [reply_attachment]\n{pn} [--p] [reply_attachment]\n{pn} [--dc] reply or add link image\n{pn} [--sl] [reply_attachment]\n{pn} [--imgur] [reply_attachment]"
   },
 
-  onStart: async function ({ message, args, event }) {
+  onStart: async ({ message, args, event }) => {
     try {
-      let { messageReply, type, senderID } = event;
-      let num = 0;
-      let length = messageReply.attachments.length;
-      var msg = `✅ | 𝙷𝚎𝚛𝚎 𝚒𝚜 𝚢𝚘𝚞𝚛 ${length} 𝚏𝚒𝚕𝚎 𝚊𝚝𝚝𝚊𝚌𝚑𝚎𝚍 <😘\n\n`;
-      if (
-        args[0] === "--t" ||
-        args[0] === "t" ||
-        args[0] === "tinyurl" ||
-        args[0] == "-t"
-      ) {
-        if (type !== "message_reply") {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        } else {
-          for (let i = 0; i < length; i++) {
-            let shortLink = await shortenURL(messageReply.attachments[i].url);
-            num += 1;
-            msg += `${num}: ${shortLink}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "i" ||
-        args[0] == "--i" ||
-        args[0] == "imgbb" ||
-        args[0] == "-i"
-      ) {
-        if (
-          type !== "message_reply" &&
-          !["photo", "sticker"].includes(messageReply.attachments[i]?.type)
-        ) {
-          return message.reply("❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚙𝚑𝚘𝚝𝚘");
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply("❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚙𝚑𝚘𝚝𝚘");
-        } else {
-          for (let i = 0; i < length; i++) {
-            let imgLink = await get(
-              `${await baseApiUrl()}/imgbb?url=${encodeURIComponent(messageReply.attachments[i].url)}`,
-            );
-            num += 1;
-            msg += `${num}: ${imgLink.data.data.url}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "tg" ||
-        args[0] == "telegraph" ||
-        args[0] == "-tg" ||
-        args[0] == "--tg"
-      ) {
-        if (type !== "message_reply") {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        } else {
-          for (let i = 0; i < length; i++) {
-            let shortLink = await shortenURL(messageReply.attachments[i].url);
-            const res = await get(`${await baseApiUrl()}/tg?url=${shortLink}`);
-            num += 1;
-            msg += `${num}: ${res.data.data}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "imgur" ||
-        args[0] == "imgurl" ||
-        args[0] == "-imgur" ||
-        args[0] == "--imgur"
-      ) {
-        if (type !== "message_reply") {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        } else {
-          for (let i = 0; i < length; i++) {
-            let shortLink = await shortenURL(messageReply.attachments[i].url);
-            const res = await get(
-              `${await baseApiUrl()}/imgur?url=${shortLink}`,
-            );
-            num += 1;
-            msg += `${num}: ${res.data.data}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "dc" ||
-        args[0] == "discord" ||
-        args[0] == "-d" ||
-        args[0] == "--dc"
-      ) {
-        if (type !== "message_reply") {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        } else {
-          for (let i = 0; i < length; i++) {
-            const encLink = encodeURIComponent(messageReply.attachments[i].url);
-            const res = await get(
-              `${await baseApiUrl()}/dc?imageUrl=${encLink}`,
-            );
-            num += 1;
-            msg += `${num}: ${res.data.url}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "sl" ||
-        args[0] == "shortlink" ||
-        args[0] == "-s" ||
-        args[0] == "--sl"
-      ) {
-        if (type !== "message_reply") {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        } else {
-          for (let i = 0; i < length; i++) {
-            const { data } = await get(
-              `${await baseApiUrl()}/linkshort?link=${encodeURIComponent(messageReply.attachments[i].url)}name=${encodeURIComponent(messageReply.attachments[i].filename)}`,
-            );
-            num += 1;
-            msg += `${num}: ${data.shortLink}\n`;
-          }
-          message.reply(msg);
-        }
-      } else if (
-        args[0] == "--p" ||
-        args[0] == "postimg" ||
-        args[0] == "postimage" ||
-        args[0] == "-p"
-      ) {
-        if (
-          type !== "message_reply" &&
-          !["photo", "sticker"].includes(messageReply.attachments[i]?.type)
-        ) {
-          return message.reply("❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚙𝚑𝚘𝚝𝚘");
-        }
-        if (!messageReply.attachments || length == 0) {
-          return message.reply("❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚙𝚑𝚘𝚝𝚘");
-        } else {
-          for (let i = 0; i < length; i++) {
-            const encLink = encodeURIComponent(messageReply.attachments[i].url);
-            const res = await get(
-              `${await baseApiUrl()}/postimg?imageUrl=${encLink}`,
-            );
-            num += 1;
-            msg += `${num}: ${res.data.directLink}\n`;
-          }
-          message.reply(msg);
-        }
+      const { messageReply, type } = event;
+      const attachments = messageReply?.attachments || [];
+      const length = attachments.length;
+      if (length === 0) {
+        return message.reply(
+          "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 reply to a video, audio, or photo"
+        );
       }
 
-      if (!args[0]) {
-        if (type !== "message_reply")
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        else if (!messageReply.attachments || length == 0)
-          return message.reply(
-            "❌ | 𝚈𝚘𝚞 𝚖𝚞𝚜𝚝 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚊𝚞𝚍𝚒𝚘, 𝚟𝚒𝚍𝚎𝚘, 𝚘𝚛 𝚙𝚑𝚘𝚝𝚘",
-          );
-        else if (
-          (type == "message_reply" && messageReply.attachments) ||
-          length !== 0
-        ) {
-          for (let i = 0; i < length; i++) {
-            num += 1;
-            msg += `${num}: ${messageReply.attachments[i].url}\n\n`;
-          }
-          message.reply(msg);
+      let msg = `✅ | Here are your ${length} link(s):\n\n`;
+      let api = await baseApiUrl();
+
+      const doShorten = async (url) => await shorten(url);
+
+      for (let i = 0; i < length; i++) {
+        const url = attachments[i].url;
+        let link = "";
+
+        const opt = args[0];
+        switch (opt) {
+          case "--t":
+          case "t":
+          case "tinyurl":
+            link = await doShorten(url);
+            break;
+
+          case "--tg":
+          case "tg":
+          case "telegraph":
+            {
+              const s = await doShorten(url);
+              const res = await get(`${api}/tg?url=${s}`);
+              link = res.data.data;
+            }
+            break;
+
+          case "--i":
+          case "i":
+          case "imgbb":
+            {
+              const res = await get(
+                `${api}/imgbb?url=${encodeURIComponent(url)}`
+              );
+              link = res.data.data.url;
+            }
+            break;
+
+          case "--imgur":
+          case "imgur":
+          case "imgurl":
+            {
+              const s = await doShorten(url);
+              const res = await get(`${api}/imgur?url=${s}`);
+              link = res.data.data;
+            }
+            break;
+
+          case "--p":
+          case "postimg":
+          case "postimage":
+            {
+              const res = await get(
+                `${api}/postimg?imageUrl=${encodeURIComponent(url)}`
+              );
+              link = res.data.directLink;
+            }
+            break;
+
+          case "--dc":
+          case "dc":
+          case "discord":
+            {
+              const res = await get(
+                `${api}/dc?imageUrl=${encodeURIComponent(url)}`
+              );
+              link = res.data.url;
+            }
+            break;
+
+          case "--sl":
+          case "sl":
+          case "shortlink":
+            {
+              const res = await get(
+                `${api}/linkshort?link=${encodeURIComponent(
+                  url
+                )}&name=${encodeURIComponent(attachments[i].filename)}`
+              );
+              link = res.data.shortLink;
+            }
+            break;
+
+          default:
+            // no args: return direct URL
+            link = url;
+            break;
         }
+
+        msg += `${i + 1}: ${link}\n`;
       }
+
+      message.reply(msg);
     } catch (err) {
-      console.log(err);
-      message.reply(`❎ | 𝙴𝚛𝚛𝚘𝚛: ${err.message}`);
+      console.error(err);
+      message.reply(`❎ | Error: ${err.message}`);
     }
-  },
+  }
 };
