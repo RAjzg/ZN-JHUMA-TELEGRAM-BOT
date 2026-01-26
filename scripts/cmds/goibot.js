a9a7464df91e04437b65a8d5aacfea1152ca00d1
ZN JHUMA <mdnurnobihaque79@gmail.com>
2026-01-26 23:26:51 +0600

    Update goibot.js

--- scripts/cmds/goibot.js
+++ scripts/cmds/goibot.js
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.onChat = async ({ message, event }) => {
   const hh = event.text?.toLowerCase() || "";
-  if (hh.startsWith("bot") || hh.startsWith("bby")) {
+  if (hh.startsWith("Zuma") || hh.startsWith("Jhuma")) {
     try {
       const ar = [
         "°অনুভূতি প্রকাশ করতে নেই মানুষ নাটক মনে করে মজা নেয়°..! ",
